f953aa7d939349dc07cc3808ae4dfd15ac715709
Jensen Lawless <jensenlawless@gmail.com>
2023-04-18 11:55:13 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+let names = ["Jon", "Bob", "Carl"];
+
+function writeCards(names, event) {
+    let cards = []
+
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return cards
+}
+
+writeCards(names)
+
+function countDown(nums) {
+    let i = nums;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+
+    }
+
+    return nums;
+}
+
